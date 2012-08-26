b6b7de2763b15da9eb31a7342a9e20f7e9a35cb0
Dan Merrill <danvmerrill@gmail.com>
2012-08-26 16:29:38 -0700

    Rigged human player, added run animation

--- MainBuild/Assets/Scripts/AnimationController.js
+++ MainBuild/Assets/Scripts/AnimationController.js
@@ -39,16 +39,32 @@ function Update()
 		
 		if ( forwardMotion > 0 )
 		{
-			// Adjust the animation speed to match with how fast the
-			// character is moving forward
-			t = Mathf.Clamp( Mathf.Abs( speed / maxForwardSpeed ), 0, maxForwardSpeed );
-			animationTarget[ "walk" ].speed = Mathf.Lerp( 0.25, 1, t );
+			if ( forwardMotion < 1.51 )
+			{
+				// Adjust the animation speed to match with how fast the
+				// character is moving forward
+				t = Mathf.Clamp( Mathf.Abs( speed / maxForwardSpeed ), 0, maxForwardSpeed );
+				animationTarget[ "walk" ].speed = Mathf.Lerp( 0.25, 1, t );
 										
-			if ( animationTarget.IsPlaying( "idle" ) )
-				// Don't blend coming from a land, just play
-				animationTarget.Play( "walk" ); 
+				if ( animationTarget.IsPlaying( "idle" ) )
+					// Don't blend coming from a land, just play
+					animationTarget.Play( "walk" ); 
+				else
+					animationTarget.CrossFade( "walk" );
+			}
 			else
-				animationTarget.CrossFade( "walk" );
+			{
+				// Adjust the animation speed to match with how fast the
+				// character is moving forward
+				t = Mathf.Clamp( Mathf.Abs( speed / maxForwardSpeed ), 0, maxForwardSpeed );
+				animationTarget[ "run" ].speed = Mathf.Lerp( 0.25, 1, t );
+										
+				if ( animationTarget.IsPlaying( "idle" ) )
+					// Don't blend coming from a land, just play
+					animationTarget.Play( "run" ); 
+				else
+					animationTarget.CrossFade( "run" );
+			}
 		}
 	}
 	else
