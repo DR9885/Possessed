41cd07080813a7f83495a60fad8243e5240180f8
Dan Merrill <danvmerrill@gmail.com>
2012-08-30 16:45:34 -0700

    removed c# folder

--- MainBuild/Assets/Scripts/C#/Src/CustomFiniteState.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public abstract class CustomFiniteState : ScriptableObject, IFiniteState<MonoBehaviour>
-{
-    public abstract void OnEnter(MonoBehaviour target);
-    public abstract void OnExit(MonoBehaviour target);
-    public abstract void OnDecision(MonoBehaviour target);
-}
