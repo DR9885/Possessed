c2773007315dab14847f87ea385fd852c8c74cb3
Daniel P. Rossi <DR9885@gmail.com>
2012-09-02 21:45:23 -0400

    Added Controller States

---
+++ MainBuild/Assets/Scripts/C#/Controller/States/ControllerIdleState.cs
@@ -0,0 +1,23 @@
+public class ControllerIdleState : FSMState<MasterController, TargetState>
+{
+
+    public override TargetState State
+    {
+        get { return TargetState.Idle; }
+    }
+
+    public override void Enter(MasterController entity)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Execute(MasterController entity)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Exit(MasterController entity)
+    {
+        throw new System.NotImplementedException();
+    }
+}
